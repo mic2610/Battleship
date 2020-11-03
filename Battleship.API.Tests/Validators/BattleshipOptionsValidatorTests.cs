3d3c2a31442d17f2c137f36769c80c9bf363d08d
Michael Ayoub <z1133447@wowcorp.com.au>
2020-11-03 14:32:24 +1100

    [Fluent-Validation] More unit tests

--- Battleship.API.Tests/Validators/BattleshipOptionsValidatorTests.cs
+++ Battleship.API.Tests/Validators/BattleshipOptionsValidatorTests.cs
@@ -2,9 +2,6 @@
 using Battleship.API.Models;
 using FluentValidation.TestHelper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Battleship.Api.Tests.Validators
 {
@@ -29,13 +26,26 @@ namespace Battleship.Api.Tests.Validators
                 result.ShouldHaveValidationErrorFor(bs => bs.PlayerId);
             }
 
+            [TestMethod]
+            public void WhenPropertyValuesAreInvalid()
+            {
+                var model = new BattleshipOptions { PlayerId = 0, Column = 0, Row = 0, OpponentId = 0 };
+                var result = validator.TestValidate(model);
+                result.ShouldHaveAnyValidationError();
+            }
         }
 
         [TestClass]
         public class ShouldNotHaveError
         {
+            [ClassInitialize]
+            public static void Initialise(TestContext context)
+            {
+                validator = new BattleshipOptionsValidator();
+            }
+
             [TestMethod]
-            public void ReturnsValid()
+            public void WhenPropertiesAreSet()
             {
                 var model = new BattleshipOptions { PlayerId = 1, Column = 1, Row = 1, OpponentId = 2 };
                 var result = validator.TestValidate(model);
